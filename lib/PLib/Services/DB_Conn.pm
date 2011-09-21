1a7d0d861deb9e244fd20cc1a357560697eb7db0
Sean Blanton <sean@blanton.com>
2011-09-21 04:29:53 -0500

    oops- dir mismatch

---
+++ lib/PLib/Services/DB_Conn.pm
@@ -0,0 +1,68 @@
+package PLib::Services::DB_Exec;
+
+use Moose;
+
+with 'PLib::Roles::Logger';
+
+use DBI;
+
+has sql => (
+	is => 'rw',
+);
+
+has sth => (
+	is => 'rw',
+);
+
+has dsn => (
+	is => 'rw',
+	required => 1,
+);
+
+has user => (
+	is => 'rw',
+	required => 1,
+);
+
+has pwd => (
+	is => 'rw',
+	required => 1,
+);
+
+has dbh => (
+	is => 'rw',
+	default => \&dbh_builder,
+	lazy => 1,
+);
+
+sub dbh_builder {
+ my $s = shift;
+
+ my $dbh = DBI->connect($dsn,$_[0]->user,$_[0]->pwd);
+
+ unless ( $dbh ) {
+	$s->log->error(DBI->errstr());
+	return undef;
+ }
+
+ return $dbh;
+
+}
+
+sub exec {
+	my $s = shift or confess;
+
+	$s->dbh or confess;
+	$s->sql or confess;
+
+	$s->sth = $s->dbh->prepare($s->sql);
+	$s->sth->execute or die $DBI::errstr;
+
+	return sub { $s->sth->fetchrow_hashref() }
+
+}
+
+no Moose;
+
+1;
+
