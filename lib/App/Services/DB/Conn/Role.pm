be239bf1e39d60cb17d90ac5f933da6ec1484b36
Sean Blanton <sean@blanton.com>
2013-07-16 12:39:21 -0500

    fix to role package name

--- lib/App/Services/DB/Conn/Role.pm
+++ lib/App/Services/DB/Conn/Role.pm
@@ -1,4 +1,4 @@
-package App::Services::DB::DB_Conn::Role;  #-- Log service interface
+package App::Services::DB::Conn::Role;  #-- Log service interface
 
 use Moo::Role;
 
@@ -7,7 +7,7 @@ has db_conn_svc => (
  isa => sub { ref eq 'App::Services::Service::DB_Conn' },
  handles => ['dbh'],
  required => 1,
-);
+); #--- handles => didn't work here for some reason
 
 no Moo::Role;
 
